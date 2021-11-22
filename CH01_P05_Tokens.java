228dbfb6b7dc8054d564b2db36e633d1d06abd0a
GkHell <gpkmyworld@gmail.com>
2021-11-22 19:44:51 +0530

    22/11/2021⠀⎦⎡⠀19:44⠀⎦⎢⠀Monday

---
+++ CH01_P05_Tokens.java
@@ -0,0 +1,23 @@
+
+//keyword : 
+//variable & literals:
+public class CH01_P05_Tokens{
+	
+	public static void main(String[] xcz) {
+		
+
+		System.out.println("hello "+xcz[0]);
+
+		float f = 20.4f+(float)10.1;
+		System.out.println("value of f "+f);
+
+
+		System.out.print("12345678\t90\n");
+		System.out.print("1234\f5678\r90");
+
+		int i = 10;
+
+		System.out.printf("\n\\ni = %d, f = %f\n",i,f);
+
+	}
+}
---
+++ CH02_P01_Comments.java
@@ -0,0 +1,34 @@
+
+// this
+/*
+home is here.
+
+RULE for document comment : mension it before the code
+*/
+
+/**
+ * 
+ * <h1><marquee>game te hoy baka...</marquee></h1>
+ * */
+public class CH02_P01_Comments{
+	
+	/*
+		phase 1
+	*/
+	/**
+	 * 
+	 * 	Hello There , we are in main...
+	 * */
+	public static void main(String[] args) {
+		
+		System.out.println("hell of COde....");
+	}
+
+	/**
+	 * 	<h2 style='color:red;'>abstact public method...</h2>
+	 * */
+	public abstract void show()
+	{
+
+	}
+}
