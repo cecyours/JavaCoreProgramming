f6fc04b65182b10215f7d76cb17903775cf209fd
GkHell <gpkmyworld@gmail.com>
2021-12-18 19:49:40 +0530

    18/12/2021⠀⎦⎡⠀19:49⠀⎦⎢⠀Saturday

---
+++ CH02_P12_AssignmentOperation.java
@@ -0,0 +1,14 @@
+
+class Home{
+	public static void main(String[] args) {
+		int a=10;
+
+		// a = a+3;
+		a+=3;
+		System.out.println("after += "+a);
+
+		a-=2;
+		System.out.println("after -= "+a);
+		
+	}
+}
