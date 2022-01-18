bb95888b6bfbfb7dd53e43e9d9f06a145ff76e41
gkHell <gpkmyworld@gmail.com>
2022-01-18 08:35:20 +0530

    18/01/2022⠀⎦⎡⠀08:35⠀⎦⎢⠀Tuesday

---
+++ CH05_P08_StringBuffer.java
@@ -0,0 +1,36 @@
+
+class Home{
+	public static void main(String[] args) {
+
+		StringBuffer s = new StringBuffer("12367890");
+		System.out.println("stirng "+s);
+
+		s.append("Hii");
+		System.out.println("append : "+s);
+
+		s.insert(3,"--->");
+		System.out.println("insert : "+s);
+
+		s.delete(0,5);
+		System.out.println("delete : "+s);
+
+		s.reverse();
+		System.out.println("reverse : "+s);
+
+		String str = s.toString();
+		System.out.println("toString : "+s);
+
+		 s = new StringBuffer("peter, how are you peter..");
+		 System.out.println("indexOf : "+s.indexOf("peter"));
+		 System.out.println("lastIndexOf : "+s.lastIndexOf("peter"));
+
+
+	}
+}
+// class H{
+// 	int a;
+// 	public H(int a)
+// 	{
+// 		this.a = a;
+// 	}
+// }
