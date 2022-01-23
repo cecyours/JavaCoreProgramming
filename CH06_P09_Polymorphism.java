b7cd076addf1595a7773d2d8545df7b946ebd502
gkHell <gpkmyworld@gmail.com>
2022-01-23 15:43:45 +0530

    23/01/2022⠀⎦⎡⠀15:43⠀⎦⎢⠀Sunday

---
+++ CH06_P09_Polymorphism.java
@@ -0,0 +1,28 @@
+class CH06_P09_Polymorphism  {
+	public static void main(String[] args) {
+		
+		Code code = new Code();
+		int a=19,b=3,c=10;
+
+		System.out.println("Sum : "+code.sum());
+		System.out.println("Sum : "+code.sum(a,c));
+		System.out.println("Sum : "+code.sum(a,a,b));
+
+	}
+}
+class Code{
+
+	int sum()
+	{
+		return 0;
+	}
+	int sum(int a,int b)
+	{
+		return a+b;
+	}
+
+	int sum(int a,int b,int c)
+	{
+		return a+b+c;
+	}
+}
